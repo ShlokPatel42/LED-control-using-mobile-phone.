615ec1e2e4e7458d1c5efb2c10b56f78b88e1b06
ShlokPatel42 <patelshlok369@gmail.com>
2025-11-09 03:28:49 +0530

    Add files via upload

---
+++ lediot.ino
@@ -0,0 +1,45 @@
+#define BLYNK_AUTH_TOKEN "T35gExnpQ9H8AyxPxNtCf6KA2rAqPvgV"
+#define BLYNK_TEMPLATE_ID "TMPL3KvRlT9SR"
+#define BLYNK_TEMPLATE_NAME "Quickstart Device"
+
+#define BLYNK_PRINT Serial
+
+
+
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiSTA.h>
+#include <WiFiScan.h>
+#include <WiFiServer.h>
+#include <WiFiType.h>
+#include <WiFiUdp.h>
+
+#include <BlynkSimpleEsp32.h>
+
+#define WiFi_SSID "realme GT 6"
+#define WiFi_PASS "q5y6j9np"
+
+
+const int lpin = 25;
+
+BLYNK_WRITE(V0)
+{
+  int lstate = param.asInt();
+  Serial.println(lstate);
+  digitalWrite(lpin, lstate);
+}
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(lpin, OUTPUT);
+  WiFi.begin(WiFi_SSID, WiFi_PASS);
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, WiFi_SSID, WiFi_PASS);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run();
+}
